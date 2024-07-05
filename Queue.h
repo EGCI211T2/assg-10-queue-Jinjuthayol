a953fc25181732046ff8e146ff24ca318ccff223
Jinjuthayol <168159170+Jinjuthayol@users.noreply.github.com>
2024-07-05 03:36:07 +0000

    .

--- Queue.h
+++ Queue.h
@@ -2,6 +2,7 @@
 #ifndef queue_h
 #define queue_h
 #include "Node.h"
+#include <cstring>
 class Queue {
 	NodePtr headPtr,tailPtr;
 	int size;
@@ -15,23 +16,31 @@ public:
 
 void Queue::enqueue(int x){
   NodePtr new_node= new NODE(x);
-if(new_node){ 
-    /* Add head and tail for me please */
-   1. connect & Change tail
-  2. (may be) change head  when the queue is empty
-  3. increase size*/
-	
- }
+if(new_node){
+    if(size==0) {
+      headPtr=new_node;
+      tailPtr=headPtr;
+    }
+    else{
+        tailPtr->set_next(new_node);
+    }
+        tailPtr=new_node;
+        size++;
+   }
 }
 
 int Queue::dequeue(){
   if(headPtr!=NULL){
      NodePtr t=headPtr;
      int value= t->get_value();
-     /* Add head and tail for me please */
-          
+     headPtr=headPtr->get_next();
+     if(size==1){
+      tailPtr=NULL;
+     }    
+     size--;
      delete t;
      return value;
+
   }
   cout<<"Empty queue";
   return -1;
@@ -40,11 +49,14 @@ int Queue::dequeue(){
 
 Queue::Queue(){
     //initialize Queue
+    size=0;
+    headPtr=NULL;
+    tailPtr=NULL;
     
 }
 Queue::~Queue(){
-    //delete all remaning Queue (i.e. DQ all) 
-    
+  cout<<"clearing queue"<<endl;
+  dequeue();
 }
 
 
